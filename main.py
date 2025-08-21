4b4a663870faa490bb81ba84b3dc670d5c51626e
Kyri <kyriacoskonis@outlook.com>
2025-08-21 20:40:43 +0000

    planets.py ready

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Solar System Data Visualiser core code.
+
+Interactive Visualiser (2D) of planet's positions for a chosen date.
+Uses NASA/JPL-style Keplerian elements reference to J2000.0.
+--------
+
+to start
+"""
